957bccb169f9f066dc42782fa0f676e483ed0381
Anand Sandilya <seanjin17@gmail.com>
2022-02-08 19:00:35 +0530

    feat: create upload pdf handler

--- src/uploadPdf.ts
+++ src/uploadPdf.ts
@@ -1,10 +1,12 @@
 import { successResponse } from './utils';
 import { Response } from './utils';
 
-const hello = async (event: AWSLambda.APIGatewayEvent): Promise<Response> => {
+const uploadPdf = async (
+  event: AWSLambda.APIGatewayEvent
+): Promise<Response> => {
   return successResponse({
     message: 'Go Serverless! Your function executed successfully!',
     input: event,
   });
 };
-export default hello;
+export default uploadPdf;
